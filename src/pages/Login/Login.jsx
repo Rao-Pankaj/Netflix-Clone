f55657622b98569c73931158bc68d5336fabfe0e
Pankaj Rao <116181375+Rao-Pankaj@users.noreply.github.com>
2025-11-20 13:46:32 +0530

    Initial commit

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import './Login.css'
+import logo from '../../assets/logo.png'
+
+const Login = () => {
+
+const [signState, setSignState]= useState("Sign In")
+
+
+
+  return (
+    <div className='login'>
+      <img src={logo} className='login-logo' alt="" />
+      <div className="login-form">
+        <h1>{signState}</h1>
+        <form>
+          {signState==="Sign Up"?<input type="text" placeholder='Your name' />:<></>}
+          <input type="email" placeholder='Email or phone number' />
+          <input type="password" placeholder='Password' />
+          {signState==="Sign Up"?<input type="password" placeholder='Confirm Password' />:<></>}
+          <button className='btn'>{signState}</button>
+          <div className="form-help">
+          <div className="remember">
+            <input type="checkbox" id="remember" />
+            <label htmlFor="remember">Remember me</label>
+          </div>
+          <p>Need Help?</p>
+          </div> 
+        </form>
+        <div className="form-switch">
+          {signState==="Sign In"? <p>New to Netflix? <span onClick={()=>{setSignState("Sign Up")}}>Sign Up Now</span></p>
+          :<p>Already have account? <span onClick={()=>{setSignState("Sign In")}}>Sign In Now</span></p>
+  }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
